74b8e60e165798f64106cc40b004603f951f3937
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-15 03:35:24 +0500

    Addition - Slides are added

--- Kotlin Flow With Jetpack Compose/app/src/main/java/com/android/flowwithcompose/MainViewModel.kt
+++ Kotlin Flow With Jetpack Compose/app/src/main/java/com/android/flowwithcompose/MainViewModel.kt
@@ -3,10 +3,7 @@ package com.android.flowwithcompose
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
@@ -35,6 +32,7 @@ class MainViewModel : ViewModel() {
         }
     }
 
+
     // It will always collect the latest value.
     // Incase the collect block takes too much time to complete its execution and it will receive
     // a new new value the rest execution will be canceled
@@ -50,13 +48,101 @@ class MainViewModel : ViewModel() {
 
     private fun collectFlowWithFilter() {
         viewModelScope.launch {
-            countDownFlow.
-            filter { time ->
+            countDownFlow.filter { time ->
                 time % 2 == 0
-            }.
-            collect { time ->
+            }.collect { time ->
                 println("Time is $time")
             }
+        }
+    }
+
+
+    private fun collectFlowWithFilterAndMap() {
+        viewModelScope.launch {
+            countDownFlow.filter { time ->
+                time % 2 == 0
+            }.map { time ->
+                time * time
+            }.collect { time ->
+                println("Time is $time")
+            }
+        }
+    }
+
+
+    private fun collectFlowWithFilterAndMapAndOnEach() {
+        viewModelScope.launch {
+            countDownFlow.filter { time ->
+                time % 2 == 0
+            }.map { time ->
+                time * time
+            }.onEach { time ->   // OnEach will not end the flow as collect do so
+                println("Time is $time")
+            }.collect { time ->
+                println("Time is $time")
+            }
+        }
+    }
+
+
+    private fun collectFlowWithOnEach() {
+        countDownFlow.onEach { time ->   // OnEach launch similar like collect
+            println("Time is $time")
+        }.launchIn(viewModelScope)
+    }
+
+
+    private fun collectFlowWithCount() {
+        viewModelScope.launch {
+            val count = countDownFlow.count { time ->
+                time % 2 == 0
+            }
+            println("Count is $count")
+        }
+    }
+
+
+    private fun collectFlowWithReduce() {
+        viewModelScope.launch {
+            val result = countDownFlow.reduce { accumulator , value ->
+                accumulator + value
+            }
+            println("Result is $result")
+        }
+    }
+
+
+    private fun collectFlowWithFold() {
+        viewModelScope.launch {
+            val result = countDownFlow.fold(100) { accumulator , value ->
+                accumulator + value
+            }
+            println("Result is $result")
+        }
+    }
+
+
+    // Flatten flows, if we have two flows then we can combine these to have the results coming in a single flow.
+
+    private fun flattenFlow() {
+        val flow1 = flow<Int> {
+            emit(1)
+            delay(500)
+            emit(2)
+        }
+
+        val flow2 = flow<Int> {
+            emit(1)
+            delay(500)
+            emit(2)
+            delay(500)
+            emit(3)
+        }
+        viewModelScope.launch {
+            val result = countDownFlow.fold(100) { accumulator , value ->
+                accumulator + value
+            }
+            println("Result is $result")
         }
     }
 }
